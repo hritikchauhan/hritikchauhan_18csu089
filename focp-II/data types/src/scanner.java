fb031e448b27971cd2d324648545c7512c83cd9e
hritik chauhan <hritikchauhan0@gmail.com>
2019-01-11 10:49:15 +0530

    datatypes

---
+++ focp-II/data types/src/scanner.java
@@ -0,0 +1,18 @@
+import java.util.*
+class integer
+{
+	public static void main(String args[])
+	{
+		 int a=1,b=2,add,sub,div,mod,mul;
+		 add=a+b;
+		 sub=a-b;
+		 div=a/b;
+		 mod=a%b;
+		 mul=a*b;
+		 System.out.println(add);
+		 System.out.println(sub);
+		 System.out.println(mul);
+		 System.out.println(div);
+		 System.out.println(mod);
+	}
+}
